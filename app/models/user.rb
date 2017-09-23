0cf1b11fc9c51a49143467aa74d3c7357f2c209c
bingo8670 <307286130@qq.com>
2017-09-23 18:45:17 +0800

    219 6.3.3 done

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase}
+  #before_save { email.downcase! } 另一种实现方式
+
+  validates :name, presence: true,  length: { maximum: 50 }
+  #省略了括号，validates只是方法,等同于  validates(:name, presence: true)
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  #邮箱格式
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+end
