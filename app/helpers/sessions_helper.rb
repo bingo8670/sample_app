acbf37ab25dfc5483f76d74f757fefeca95a955d
bingo8670 <307286130@qq.com>
2017-10-16 14:26:53 +0800

    289 8.2.3 done

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,16 @@
+module SessionsHelper
+  #登入指定的用户
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #返回当前登陆的用户（如果有的话）
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+    #  @foo = @foo || "bar" 和 @foo ||= "bar" 两种写法是等效的。
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+end
