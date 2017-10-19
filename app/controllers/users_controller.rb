ef8eae0b7fd52105d3888825131e56cd03d52da6
bingo8670 <307286130@qq.com>
2017-10-19 23:11:18 +0800

    355 10.3.1 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :udpate]
+  before_action :logged_in_user, only: [:index, :edit, :udpate]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @user = User.all
+  end
 
   def new
     @user = User.new
